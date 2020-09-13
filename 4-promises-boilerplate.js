19f1ba8032ab46fbfdfb822f174f44ef5666915c
Matt McKenzie <matthew.mitchell.mckenzie@gmail.com>
2020-09-13 23:07:12 +1000

    minor spelling change

--- 4-promises-boilerplate.js
+++ 4-promises-boilerplate.js
@@ -11,6 +11,7 @@ const funcObj = require('promise-source')
 // 1. wordQuery is a function that takes ONE string argument, and returns a promise. Because it returns a promise, we can chain a .then on to it, and then deal with the data that is returned inside the .then. .then takes a callback, and that callback will be given one argument. Run this function on several input strings to see if you can guess what wordQuery does behind the scenes. Try entering in the wrong number of arguments, and observe the output.
 funcObj.wordQuery()
     .then(function(x) {
+        
     })
 
 // 2. arrWeirdness expects a string as the first argument, and an array as the second argument. If you don't provide one you will get an error, and this will be sent through to the .catch. If you do send a string and an array as the correct arguments, you will get a result. See if you can work out what produces that result. Test this function to see what error messages you get when you do not play nice.
@@ -26,10 +27,8 @@ funcObj.arrWeirdness()
 // funcObj.whatArgs()
 
 // 4. This function takes string arguments, and returns a promise. See if you can make sense of what is going on, and have a play with the function to try to understand it. 
-funcObj.isAWhatNow("clue", "this")
-    .then(x => {
-        console.log(x)
-    })
+// funcObj.isAWhatNow()
+
 
 // 5. Very difficult. See if you can make it to the center of the earth.
 // funcObj.rabbitHole()
